9c4cac8c95b5c955ffb2ab9751c73e4b96a1c2d7
valek90@bk.ru <valek90@bk.ru>
2023-03-01 13:28:23 +0300

    first commit

---
+++ Homework/Homework1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int reversA = numberA % 10;
+int remeberA = numberA;
+while (numberA / 10 > 0)
+
+{
+    reversA = reversA * 10 + numberA / 10 % 10;
+    numberA = numberA / 10;
+}
+if (remeberA == reversA)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
---
+++ Homework/Homework2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в
+// 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите число x1");
+int numberx1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число y1");
+int numbery1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число z1");
+int numberz1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число x2");
+int numberx2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число y2");
+int numbery2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число z2");
+int numberz2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((numberx2 - numberx1) * (numberx2 - numberx1) + (numbery2 - numbery1) * (numbery2 - numbery1) + (numberz2 - numberz1) * (numberz2 - numberz1));
+result = Math.Round(result,2);  // Это округление до двух знаков после запятой.
+Console.Write(result);
+
